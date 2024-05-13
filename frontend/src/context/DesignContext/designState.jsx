1e56cca69361bb90940c92a65f0d902ce7456bed
J0SAL <joyalmeida321@gmail.com>
2024-05-14 01:01:13 +0530

    added dynamic input ouput

--- frontend/src/context/DesignContext/designState.jsx
+++ frontend/src/context/DesignContext/designState.jsx
@@ -7,17 +7,16 @@ const text = `
 flowchart LR\nsubgraph Library Management System\n    A[Start]\n    B[Add Book] --> C[Add Book to Database]\n    C --> D[Add Book to Shelf]\n    E[Borrow Book] --> F[Check if Book is Available]\n    F[Yes] --> G[Lend Book]\n    F[No] --> H[Inform User]\n    I[Return Book] --> J[Update Database]\n    J[Yes] --> K[Return Book to Shelf]\n    J[No] --> L[Inform User]\n    M[End]\nend\n
 `;
 const GEMENI_KEY = process.env.REACT_APP_GEMENI_KEY ?? "";
+const BASE_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + GEMENI_KEY;
+const headers = {
+    "Content-Type": "application/json"
+}
 
 
 function DesignState({ children }) {
-
-    const BASE_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + GEMENI_KEY;
-    const headers = {
-        "Content-Type": "application/json"
-    }
-
     const [mermaidData, setMermaidData] = useState(text);
-    const getMermaidData = async (inputtext) => {
+    const getMermaidData = async (inputData) => {
+        const inputtext = "Generate mermaid js \'" + inputData.chartType + " code\' for \'" + inputData.chartInput + "\'";
         const formData =
         {
             "contents": [
@@ -34,8 +33,11 @@ function DesignState({ children }) {
         await axios
             .post(BASE_URL, formData, headers)
             .then(function (response) {
-                const res = response.data;
-                setMermaidData(res.candidates[0].content.parts[0].text);
+                let res = response.data;
+                res = res.candidates[0].content.parts[0].text;
+                res = res.substr(res.indexOf("\n", res.indexOf("```mermaid")) + 1);
+                res = res.substr(0, res.length - 3);
+                setMermaidData(res);
             })
             .catch(function (error) {
                 console.log(error);
