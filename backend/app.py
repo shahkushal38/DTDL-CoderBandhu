3f4d71f043b17736feb9a5f15f016c2cc854eb3f
Mansi Dwivedi <dwivedimav@gmail.com>
2024-05-18 19:58:01 +0530

    lakshay complete

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,13 @@ import os
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from flask_pymongo import pymongo
+from groq import Groq
+import requests
 
 load_dotenv()
+
+GEMINI_MODEL_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_MODEL_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key="+GEMINI_MODEL_KEY
 
 app = Flask(__name__)
 
@@ -26,12 +31,51 @@ def lakshay_bot():
     modified_content = content.format(
         user_input=user_input
     )
-    cursor = user_collection.find()
-    # Iterate over the cursor to access the documents
-    for document in cursor:
-        print(document)
-    return jsonify({"userinput":modified_content})
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": modified_content,
+            }
+        ],
+        model="llama3-70b-8192",
+    )
+    
+     
+    # headers = {"Content-Type": "application/json"}
+    
+
+    # data = {
+    #     "contents": [
+    #         {
+    #             "parts": [
+    #                 {
+    #                     "text": modified_content
+    #                 }
+    #             ]
+    #         }
+    #     ],
+    #     "safety_settings": [
+    #         {
+    #             "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    #             "threshold": "BLOCK_NONE"
+    #         }
+    #     ]
+    # }
+    
+    # response = requests.post(GEMINI_MODEL_URL, headers=headers, json=data)
+    # response = response.json()
+    # text = response["candidates"][0]["content"]["parts"][0]["text"]
+
+    # cursor = user_collection.find()
+    # # Iterate over the cursor to access the documents
+    # for document in cursor:
+    #     print(document)
+    return jsonify({"input":modified_content,"output":chat_completion.choices[0].message.content})
+    # return jsonify({"input":modified_content,"output":text})
+    
 
 if __name__ == '__main__':
-    user_collection = db_conn()
+    user_collection = db_conn() 
+    client = Groq(api_key=os.getenv("GROQ_API_KEY"))
     app.run(debug=True)
